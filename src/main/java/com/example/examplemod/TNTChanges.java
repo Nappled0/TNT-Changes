5c247668bd22650b193b350379a9dd651fe3e562
Nappledo <Nappledo@github.com>
2021-02-04 22:01:24 -0300

    Updated Main Class

---
+++ src/main/java/com/example/examplemod/TNTChanges.java
@@ -0,0 +1,26 @@
+package com.example.examplemod;
+
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("tntchanges")
+public class TNTChanges
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public TNTChanges() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
